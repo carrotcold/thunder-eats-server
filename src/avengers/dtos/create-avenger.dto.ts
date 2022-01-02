67bcd6288271cbfdeae0c643ea4b2487b2530d8f
Soin Na <thunder.bolt@kakaostyle.com>
2022-01-02 19:53:10 +0900

    feat: validate create dto

--- src/avengers/dtos/create-avenger.dto.ts
+++ src/avengers/dtos/create-avenger.dto.ts
@@ -1,17 +1,23 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 // @InputType() input을 하나의 object 형태로
 @ArgsType() // 각각의 arguments로
 export class CreateAvengerDto {
   @Field(type => String)
+  @IsString()
+  @Length(2, 10)
   name: string;
 
   @Field(type => Boolean)
+  @IsBoolean()
   isHuman: boolean;
 
   @Field(type => Boolean)
+  @IsBoolean()
   isTransformable: boolean;
 
   @Field(type => String)
+  @IsString()
   weapon: string;
 }
