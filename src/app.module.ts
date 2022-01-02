3ea8c5ece0703eb30226fea76dc45abde621d3ae
Soin Na <thunder.bolt@kakaostyle.com>
2022-01-02 18:53:18 +0900

    fix: change constructor-options (schema can be generated on-the-fly in memory)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { AvengersModule } from './avengers/avengers.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // (code-first approach)
+      autoSchemaFile: true, // https://docs.nestjs.com/graphql/quick-start#code-first
       // playground: false,
       // debug: false,
       // more options - https://www.apollographql.com/docs/apollo-server/v2/api/apollo-server/#constructor-options-lt-ApolloServer-gt
