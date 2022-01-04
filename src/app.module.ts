6c7bc1a9c30d10ca191e3fb944ce6e39db820008
Soin Na <thunder.bolt@kakaostyle.com>
2022-01-05 02:30:26 +0900

    chore: clean up practice directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
-import { AvengersModule } from './avengers/avengers.module';
-import { Avenger } from 'src/avengers/entities/avenger.entity';
 
 @Module({
   imports: [
@@ -13,7 +11,6 @@ import { Avenger } from 'src/avengers/entities/avenger.entity';
       envFilePath: process.env.NODE_ENV === 'development' ? '.env.development.local' : '.env.test.local',
       ignoreEnvFile: process.env.NODE_ENV === 'production',
       validationSchema: Joi.object({
-        // https://docs.nestjs.com/techniques/configuration#schema-validation
         NODE_ENV: Joi.string().valid('development', 'production', 'test').required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
@@ -31,16 +28,11 @@ import { Avenger } from 'src/avengers/entities/avenger.entity';
       database: process.env.DB_DATABASE,
       synchronize: process.env.NODE_ENV !== 'production',
       logging: process.env.NODE_ENV !== 'production',
-      entities: [Avenger],
-      // more options 1 -  https://typeorm.io/#/connection-options/common-connection-options
-      // more options 2 -  https://typeorm.io/#/connection-options/postgres--cockroachdb-connection-options
+      entities: [],
     }),
     GraphQLModule.forRoot({
-      autoSchemaFile: true, // https://docs.nestjs.com/graphql/quick-start#code-first
-      // debug: false,
-      // more options - https://www.apollographql.com/docs/apollo-server/v2/api/apollo-server/#constructor-options-lt-ApolloServer-gt
+      autoSchemaFile: true,
     }),
-    AvengersModule,
   ],
   controllers: [],
   providers: [],
