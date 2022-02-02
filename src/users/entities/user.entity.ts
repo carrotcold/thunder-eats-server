7e73c8acf5f0d6df197d675d1117bcee55e9e00f
Soin Na <thunder.bolt@kakaostyle.com>
2022-02-02 23:54:47 +0900

    feat: hashing password with bcrypt

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,7 @@
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
 import { Field, InputType, ObjectType, registerEnumType } from '@nestjs/graphql';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
 import { CoreEntity } from '@src/common/entities/core.entity';
 
 enum UserRole {
@@ -24,4 +26,15 @@ export class User extends CoreEntity {
   @Column({ type: 'enum', enum: UserRole })
   @Field(is => UserRole)
   role: UserRole;
+
+  // https://typeorm.io/#/listeners-and-subscribers/beforeinsert
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
+  }
 }
